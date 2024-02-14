192c4dbae923074ba43e22f91d7d40299c65a3e1
WITOLD BARCINSKI <wbarcinski@hotmail.com>
2024-02-14 15:23:30 -0600

    got yellowberries

--- Portal/Program.cs
+++ Portal/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.mydomain.com/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://api.yellowberries.com/") });
 
 await builder.Build().RunAsync();
